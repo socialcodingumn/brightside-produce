76032c6b48d0884a18de0ed1e0f9ef43fa392f8d
Seth Stoxen <stoxe004@umn.edu>
2018-09-26 18:00:13 -0500

    removing MORE store references

--- app.py
+++ app.py
@@ -7,7 +7,6 @@ from flask_jwt import JWT
 from security import authenticate, identity
 from resources.user import UserRegister
 from resources.item import Item, ItemList
-from resources.store import Store, StoreList
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get(
